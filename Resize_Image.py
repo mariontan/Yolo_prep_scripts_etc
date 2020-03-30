94d891e79ab24b523c79acd4cf0b7412ff7fecd8
AIC-WS1\Xander Cruz <xander.m.cruz@gmail.com>
2020-03-30 17:46:27 +0800

    added moved height to a variable

--- Resize_Image.py
+++ Resize_Image.py
@@ -9,8 +9,7 @@ Created on Tue Mar 17 10:27:20 2020
 from PIL import Image
 import os, sys
 
-#path = r"D:/Ivan/Test_data/Stanford_AI_cars_modified/train/motorcycle/"
-#path = r"D:\Ivan\Test_data\IvanMadeDataSet\Yolo_Cars_Front_SlightSide_View\extraImages\cars"
+#path = r"D:/Ivan/Test_data/IvanMadeDataSet/Yolo_Cars_Front_SlightSide_View/train/0/"
 path = r"D:/Ivan/Test_data/IvanMadeDataSet/Stanford_AI_cars_modified/test/buses/"
 dirs = os.listdir( path )
 
@@ -18,17 +17,19 @@ dirs = os.listdir( path )
 def resize():
     for item in dirs:
         if os.path.isfile(path+item):
+            print(item)
             im = Image.open(path+item)
             width, height = im.size
             if width > 600 or height > 600:
                 aspectRatio = width/height
                 width = 600
+                height = int(width/aspectRatio)
 #                print(aspectRatio)
 #                print(width)
 #                print(300/aspectRatio)
             
             f, e = os.path.splitext(path+'/resized/'+item)
-            imResize = im.resize((width,int(width/aspectRatio)), Image.ANTIALIAS).convert('RGB')
+            imResize = im.resize((width,height), Image.ANTIALIAS).convert('RGB')
             imResize.save(f + '.jpg', 'JPEG', quality=90)
 
 resize()
