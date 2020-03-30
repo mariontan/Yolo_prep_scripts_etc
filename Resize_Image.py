75c95f2fb042e64b97bdd1b1af26dc603de7071f
AIC-WS1\Xander Cruz <xander.m.cruz@gmail.com>
2020-03-30 17:17:17 +0800

    resize image maintains apsect ratio

--- Resize_Image.py
+++ Resize_Image.py
@@ -10,16 +10,25 @@ from PIL import Image
 import os, sys
 
 #path = r"D:/Ivan/Test_data/Stanford_AI_cars_modified/train/motorcycle/"
-path = r"D:/Ivan/Test_data/Truck_Google_search/"
+#path = r"D:\Ivan\Test_data\IvanMadeDataSet\Yolo_Cars_Front_SlightSide_View\extraImages\cars"
+path = r"D:/Ivan/Test_data/IvanMadeDataSet/Stanford_AI_cars_modified/test/buses/"
 dirs = os.listdir( path )
 
+#aspect ratio width:height
 def resize():
     for item in dirs:
         if os.path.isfile(path+item):
             im = Image.open(path+item)
-            print(item)
+            width, height = im.size
+            if width > 600 or height > 600:
+                aspectRatio = width/height
+                width = 600
+#                print(aspectRatio)
+#                print(width)
+#                print(300/aspectRatio)
+            
             f, e = os.path.splitext(path+'/resized/'+item)
-            imResize = im.resize((352,352), Image.ANTIALIAS).convert('RGB')
+            imResize = im.resize((width,int(width/aspectRatio)), Image.ANTIALIAS).convert('RGB')
             imResize.save(f + '.jpg', 'JPEG', quality=90)
 
 resize()
