cb6de3876700f9fb39d296755ff22ecf5318b804
AIC-WS1\Xander Cruz <xander.m.cruz@gmail.com>
2020-03-31 16:30:20 +0800

    copies heic file to a folder

--- Resize_Image.py
+++ Resize_Image.py
@@ -8,28 +8,35 @@ Created on Tue Mar 17 10:27:20 2020
 #!/usr/bin/python
 from PIL import Image
 import os, sys
+from shutil import copyfile
 
-#path = r"D:/Ivan/Test_data/IvanMadeDataSet/Yolo_Cars_Front_SlightSide_View/train/0/"
-path = r"D:/Ivan/Test_data/IvanMadeDataSet/Stanford_AI_cars_modified/test/buses/"
+path = r"D:/Ivan/Test_data/IvanMadeDataSet/Yolo_Cars_Front_SlightSide_View/train/0/"
+#path = r"D:/Ivan/Test_data/IvanMadeDataSet/Stanford_AI_cars_modified/test/buses/"
 dirs = os.listdir( path )
+if not os.path.exists(path+'heicPics'):
+    os.mkdir(path+'heicPics')
 
+if not os.path.exists(path+'resized/'):
+    os.mkdir(path+'resized/')
 #aspect ratio width:height
 def resize():
     for item in dirs:
-        if os.path.isfile(path+item):
-            print(item)
-            im = Image.open(path+item)
-            width, height = im.size
-            if width > 600 or height > 600:
-                aspectRatio = width/height
-                width = 600
-                height = int(width/aspectRatio)
-#                print(aspectRatio)
-#                print(width)
-#                print(300/aspectRatio)
-            
-            f, e = os.path.splitext(path+'/resized/'+item)
-            imResize = im.resize((width,height), Image.ANTIALIAS).convert('RGB')
-            imResize.save(f + '.jpg', 'JPEG', quality=90)
+        try:
+            if os.path.isfile(path+item):
+                print(item)
+                im = Image.open(path+item)
+                
+                width, height = im.size
+                if width > 600 or height > 600:
+                    aspectRatio = width/height
+                    width = 600
+                    height = int(width/aspectRatio)
+                
+                f, e = os.path.splitext(path+'resized/'+item)
+                imResize = im.resize((width,height), Image.ANTIALIAS).convert('RGB')
+                imResize.save(f + '.jpg', 'JPEG', quality=90)
+        except OSError:
+            copyfile(path+item, path+'heicPics/'+item)
 
 resize()
+
